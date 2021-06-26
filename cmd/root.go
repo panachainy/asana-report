992a86b9c937a0724c78d092c4d1e54d9b218657
Panachai <panachai.ny@gmail.com>
2021-06-26 15:22:25 +0700

    clean and update description of cli

--- cmd/root.go
+++ cmd/root.go
@@ -14,22 +14,14 @@ var cfgFile string
 
 var rootCmd = &cobra.Command{
 	Use:   "asana-report",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "for generate report of asana application",
+	Long: `Can generate report of asana application with cli.
+	So you can get all task of all your project by workspace,
+	You can get summary of task and status.`,
 }
 
 var VERSION string
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute(version string) {
 	VERSION = version
 
@@ -39,14 +31,7 @@ func Execute(version string) {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.asana-report.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
