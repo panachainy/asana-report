9191e17db036f5181a4920cfd625f4dbdb4e984c
Panachai <panachai.ny@gmail.com>
2021-07-26 00:54:59 +0700

    [test] Test_GetWorkspace

--- model/workspace.go
+++ model/workspace.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"encoding/json"
+	"log"
+)
+
 type Workspace struct {
 	Project  []Project   `json:"data"`
 	NextPage interface{} `json:"next_page"`
@@ -9,4 +14,14 @@ type Project struct {
 	Gid          string `json:"gid"`
 	Name         string `json:"name"`
 	ResourceType string `json:"resource_type"`
+}
+
+func GetWorkspaceBy(blob string) Workspace {
+	var taskResult Workspace
+
+	if err := json.Unmarshal([]byte(blob), &taskResult); err != nil {
+		log.Fatal(err)
+	}
+
+	return taskResult
 }
--- service/asana_client_test.go
+++ service/asana_client_test.go
@@ -119,3 +119,117 @@ func Test_GetSubTasks(t *testing.T) {
 		})
 	}
 }
+
+func Test_GetWorkspace(t *testing.T) {
+	InitService("http://localhost:3500")
+
+	tests := []struct {
+		name         string
+		workspaceId  string
+		token        string
+		mockFunc     func()
+		expected     model.Workspace
+		expectingErr bool
+	}{
+		{
+			name:        "All success no error",
+			workspaceId: "111",
+			token:       "",
+			mockFunc: func() {
+			},
+			expected: model.GetWorkspaceBy(`
+			{
+				"data": [
+					{
+						"gid": "1200195508767749",
+						"name": "Learning",
+						"resource_type": "project"
+					},
+					{
+						"gid": "1200485948764582",
+						"name": "Vuejs+Lalarvel",
+						"resource_type": "project"
+					},
+					{
+						"gid": "1200485948764589",
+						"name": "DevOPS",
+						"resource_type": "project"
+					},
+					{
+						"gid": "1200486577426704",
+						"name": "Golang",
+						"resource_type": "project"
+					},
+					{
+						"gid": "1200486577426721",
+						"name": "JAVA",
+						"resource_type": "project"
+					},
+					{
+						"gid": "1200485948764592",
+						"name": "Tech Lead",
+						"resource_type": "project"
+					},
+					{
+						"gid": "1200495219773252",
+						"name": "Github",
+						"resource_type": "project"
+					},
+					{
+						"gid": "1200520558756397",
+						"name": "Golang-cobra",
+						"resource_type": "project"
+					},
+					{
+						"gid": "1200556977511232",
+						"name": "asana-report",
+						"resource_type": "project"
+					},
+					{
+						"gid": "1200564492567071",
+						"name": "Vuejs",
+						"resource_type": "project"
+					},
+					{
+						"gid": "1200569493185459",
+						"name": "digital-ocean",
+						"resource_type": "project"
+					},
+					{
+						"gid": "1200575424624266",
+						"name": "read",
+						"resource_type": "project"
+					},
+					{
+						"gid": "1200195560177006",
+						"name": "Code traning",
+						"resource_type": "project"
+					},
+					{
+						"gid": "1200195560177012",
+						"name": "Gitbook",
+						"resource_type": "project"
+					},
+					{
+						"gid": "1200635348570247",
+						"name": "lint bot / line oa",
+						"resource_type": "project"
+					}
+				],
+				"next_page": null
+			}
+			`),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(tt *testing.T) {
+			tc.mockFunc()
+			tasksResult := GetWorkspace(tc.workspaceId, tc.token)
+
+			if !reflect.DeepEqual(tc.expected, tasksResult) {
+				tt.Errorf("Error, tasks expectation not met, want %+v, got %+v", tc.expected, tasksResult)
+			}
+		})
+	}
+}
