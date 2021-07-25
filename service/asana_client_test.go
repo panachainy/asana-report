876f835797aac7dbf6973a8f8be4bf582966f0b0
Panachai <panachai.ny@gmail.com>
2021-07-25 23:10:36 +0700

    [test] Test_GetSubTasks

--- service/asana_client_test.go
+++ service/asana_client_test.go
@@ -63,3 +63,59 @@ func Test_GetTasks(t *testing.T) {
 		})
 	}
 }
+
+func Test_GetSubTasks(t *testing.T) {
+	InitService("http://localhost:3500")
+
+	tests := []struct {
+		name         string
+		projectId    string
+		token        string
+		mockFunc     func()
+		expected     model.Tasks
+		expectingErr bool
+	}{
+		{
+			name:      "All success no error",
+			projectId: "111",
+			token:     "",
+			mockFunc: func() {
+			},
+			expected: model.GetTasksBy(`
+			{
+				"data": [
+					{
+						"gid": "1200653409861926",
+						"assignee": {
+							"gid": "1127028096108896",
+							"resource_type": "user"
+						},
+						"completed": true,
+						"name": "cobra-101 (part 1 setup project)"
+					},
+					{
+						"gid": "1200653409861948",
+						"assignee": {
+							"gid": "1127028096108896",
+							"resource_type": "user"
+						},
+						"completed": false,
+						"name": "cobra-101 (part 2 create command)"
+					}
+				]
+			}
+			`),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(tt *testing.T) {
+			tc.mockFunc()
+			tasksResult := GetSubTasks(tc.projectId, tc.token)
+
+			if !reflect.DeepEqual(tc.expected, tasksResult) {
+				tt.Errorf("Error, tasks expectation not met, want %+v, got %+v", tc.expected, tasksResult)
+			}
+		})
+	}
+}
